8e71cedf332e963c9eb90f7ee86770ccd1dfeffd
Thomas <thomas@craftacademy.se>
2019-01-28 18:03:10 +0100

    adds request spec for a successful rsvp to event

---
+++ spec/requests/user_can_rsvp_to_event.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+describe 'POST /events/:id/rsvp' do
+  let(:user) { create(:user) }
+  let(:event) { create(:event) }
+  let(:user_credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials) }
+
+  before do
+    get "/events/#{event.id}/rsvp", headers: headers, params: { user_id: user.id }
+  end
+
+  it 'adds user to list of attendees for event' do
+    expect(event.attendees).to include user.id
+  end
+end
