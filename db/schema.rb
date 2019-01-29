5e3c442ffe7e0cf56f9b4602f5e3337227b50a5d
camron <camronmoradi@gmail.com>
2019-01-29 10:23:12 +0100

    Creates request spec, generates Group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_194331) do
+ActiveRecord::Schema.define(version: 2019_01_29_091228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,12 @@ ActiveRecord::Schema.define(version: 2019_01_28_194331) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "attendees"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "rsvps", force: :cascade do |t|
---
+++ spec/requests/join_group_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+describe 'POST /groups/:id/members' do
+  let(:group) { create(:group) }
+
+  describe 'POST req with valid credentials' do
+    let(:user_credentials) { user.create_new_auth_token }
+    let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(user_credentials) }
+    
+    before do
+      post "/groups/#{group.id}/members", headers: headers
+    end
+
+    it 'adds user to list of members for group' do
+      members = event.members.map(&:user)
+      expect(members).to include user
+    end
+
+    it 'responds with success message' do
+      expect(response_json['message']).to eq 'You are now a member'
+    end
+
+    it 'responds with status 200' do
+      expect(response).to have_http_status 200
+    end
+  end
+end
+
+
