956f61d3acb6ca52a128561ae5527fffe9b88ba8
camron <camronmoradi@gmail.com>
2019-01-30 13:08:19 +0100

    Amends request spec

--- spec/requests/list_groups_with_its_events_spec.rb
+++ spec/requests/list_groups_with_its_events_spec.rb
@@ -5,7 +5,7 @@ describe 'GET /groups/:id' do
   let!(:group_2) { create(:group) } 
 
   let!(:events) do 
-    3.times { create(:event, group: group_1) }
+    3.times { create(:event, group: group_1, date: 1.day.from_now) }
     3.times { create(:event, group: group_2) }
   end
 
@@ -22,7 +22,7 @@ describe 'GET /groups/:id' do
     expect(response).to have_http_status(200)
   end
 
-  it 'returns 3 events' do
+  it 'returns 3 future events' do
     expect(response_json['group']['events'].count).to eq 3
   end
 end
