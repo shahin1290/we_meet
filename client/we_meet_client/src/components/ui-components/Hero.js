13d9a5a1b721b81992c350629c4ae2dc2ab0b4b3
camron <camronmoradi@gmail.com>
2019-02-03 18:51:15 +0100

    Updates hero text

--- client/we_meet_client/src/components/ui-components/Hero.js
+++ client/we_meet_client/src/components/ui-components/Hero.js
@@ -6,10 +6,10 @@ const Hero = () => {
   return (
     <Row style={{border: "1px solid rgba(0,0,0,.12)"}}>
       <Col w="1/2" text="center" p="4" height="20">
-        <div style={{ textAlign: "left", paddingLeft: "10rem", marginTop: "8rem"}}>
-          <h3 style={{fontSize: "1.5rem", color: "#4DC0B5", marginBottom: "1rem"}}>DISCOVER  |  CONNECT  |  CREATE</h3>
-          <h1 style={{fontSize: "3rem", marginBottom: "1rem"}}>Do more of what you always wanted</h1>
-          <h3 style={{fontWeight: "400", marginBottom: "1rem"}}>Meet people who share your passions and do more of what you love to do.</h3>
+        <div style={{ textAlign: "left", paddingLeft: "10rem", marginTop: "7rem"}}>
+          <h3 style={{fontSize: "1.5rem", color: "#4DC0B5", marginBottom: "1rem"}}>EXPLORE  |  CONNECT  |  CREATE</h3>
+          <h1 style={{fontSize: "2.75rem", marginBottom: "1rem"}}>Discover and participate in fun events near you</h1>
+          <h3 style={{fontWeight: "400", marginBottom: "1rem"}}>Meet people with similar interests and do more of what you love to do.</h3>
           <FillButton brand="primary">Sign up</FillButton>
         </div>
       </Col>      
