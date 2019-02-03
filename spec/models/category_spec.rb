f2827fb87e8fe94b782d4953850b70d8664c7535
Jonathan <jon1969edwards@gmail.com>
2019-02-03 16:27:09 +0100

    Writes test for category_spec, validates 'name'

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,22 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Category, type: :model do
+
+  describe 'DB table' do
+    it { is_expected.to have_db_column :name }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to has_many :groups }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+  end
+
+  describe 'Factory' do
+    it 'is valid' do
+      expect(create(:category)).to be_valid
+    end
+  end
 end
