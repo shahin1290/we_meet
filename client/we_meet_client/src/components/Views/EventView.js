c6385e1a381f8c224b0f17926c516ceb7ac74894
camron <camronmoradi@gmail.com>
2019-02-07 20:22:14 +0100

    Creates feature test for routing, updates EventView placeholder component to display event data

--- client/we_meet_client/src/components/Views/EventView.js
+++ client/we_meet_client/src/components/Views/EventView.js
@@ -5,9 +5,17 @@ class EventView extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { event: {} };
+    this.state = { event: {
+      title: '',
+      date: '',
+      time: '',
+      description: '',
+      location:'',
+      group: {
+        name: ''
+      }
+    } };
   }
-
 
   componentDidMount() {
 
@@ -32,14 +40,18 @@ class EventView extends Component {
       <>
         <div
           style={{
-            color: "red",
             fontWeight: "700",
             textAlign: "center",
             fontSize: "40px",
             marginBottom: "50px"
           }}
         >
-          This is the event page for {this.state.event.title}
+          Title: {this.state.event.title}<br></br>
+          Date: {this.state.event.date}<br></br>
+          Time: {this.state.event.time}<br></br>
+          Description:{this.state.event.description}<br></br>
+          Location: {this.state.event.location}<br></br>
+          Organized by: {this.state.event.group.name}<br></br>
         </div>
       </>
     );
@@ -48,9 +60,5 @@ class EventView extends Component {
 
 export default EventView;
 
-{
-  /* Leave the below in until we decide what to do with it */
-}
-{
-  /* <Events rsvpHandler={this.rsvp} responseMessage={this.state.containerMessage} /> */
-}
+{/* Leave the below in until we decide what to do with it */}
+{/* <Events rsvpHandler={this.rsvp} responseMessage={this.state.containerMessage} /> */}
