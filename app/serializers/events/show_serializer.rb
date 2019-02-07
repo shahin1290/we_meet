3d21c47a4920ae02620fed765a3e1350549527ae
camron <camronmoradi@gmail.com>
2019-02-07 20:44:46 +0100

    Cleans code

--- app/serializers/events/show_serializer.rb
+++ app/serializers/events/show_serializer.rb
@@ -2,7 +2,4 @@ class Events::ShowSerializer < ActiveModel::Serializer
   attributes :id, :title, :date, :time, :description, :location
   belongs_to :group, serializer: Groups::ShowSerializer
 
-  def test
-    object.group.organizer.email
-  end
 end
