6263643d41f5875fffbb43636c1bc4aab405e085
Thomas <thomas@craftacademy.se>
2019-02-08 13:17:35 +0100

    fixes factory for group with image attachment adds image_url key to group serializers modifies specs

--- app/serializers/groups/show_serializer.rb
+++ app/serializers/groups/show_serializer.rb
@@ -1,7 +1,20 @@
 class Groups::ShowSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :name, :description, :location
+  attribute :image_url
   has_many :future_events, serializer: Events::ForGroupCollectionSerializer
   has_many :past_events, serializer: Events::ForGroupCollectionSerializer
+
+  def image_url
+    if Rails.env.test?
+      rails_blob_url(object.image) if object.image.attachment
+    else
+      # we will have to update the development and production env with host. 
+      # See the test config and https://github.com/rails/rails/issues/32500 
+      object.image.service_url(expires_in: 1.hour, disposition: 'inline') if object.image.attachment
+    end
+  end
 end
 
 
