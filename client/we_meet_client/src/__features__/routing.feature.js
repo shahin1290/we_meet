f4a7315a8f5a24930ddd8ffcad1c2cba9321d7a4
camron <camronmoradi@gmail.com>
2019-02-08 20:47:57 +0100

    Updates routing feature test to be more specific

--- client/we_meet_client/src/__features__/routing.feature.js
+++ client/we_meet_client/src/__features__/routing.feature.js
@@ -13,6 +13,7 @@ describe('SPA routing', () => {
     describe('routes to views selected', () => {
         it('should show specific event view from events carousel', async () => {
             await page.click('#card')
+            await expect(page).toMatch('Hackathon at Craft Academy')
             await expect(page).toMatch('sthlm')
             await expect(page).toMatch('2019-12-12')
         });
