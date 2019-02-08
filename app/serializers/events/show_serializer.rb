9e002e3e73b94b62bf234fea5cf6345b39b2fbf0
camron <camronmoradi@gmail.com>
2019-02-08 10:35:04 +0100

    Adds attendees to Event#show serializer

--- app/serializers/events/show_serializer.rb
+++ app/serializers/events/show_serializer.rb
@@ -1,5 +1,5 @@
 class Events::ShowSerializer < ActiveModel::Serializer
   attributes :id, :title, :date, :time, :description, :location
   belongs_to :group, serializer: Groups::ShowSerializer
-
+  has_many :attendees, serializer: Users::ShowSerializer
 end
--- spec/serializers/events/show_serializer_spec.rb
+++ spec/serializers/events/show_serializer_spec.rb
@@ -2,11 +2,25 @@
 
 describe Events::ShowSerializer, type: :serializer do
   let(:sample) { create(:event) }
+  let!(:users) { 3.times { create(:user)} }
   let(:serialization) { described_class.new(sample) }
   subject { JSON.parse(serialization.to_json) }
 
+  before do
+    
+    users = User.all
+    binding.pry
+
+    users.each do | faraz |
+      sample.attendees.push(faraz)
+
+    end
+    sample.save
+  end
+
   it 'contains relevant keys' do
-    expected_keys = %w[id title date time description location group]
+    expected_keys = %w[id title date time description location group attendees]
+
     expect(subject.keys).to match expected_keys
   end
 end
