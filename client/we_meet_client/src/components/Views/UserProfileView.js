49a0e39cedb8ff947d4a16ec75ebecff4d57a461
Thomas <thomas@craftacademy.se>
2019-02-09 13:21:00 +0100

    fixes user profile logic

--- client/we_meet_client/src/components/Views/UserProfileView.js
+++ client/we_meet_client/src/components/Views/UserProfileView.js
@@ -1,26 +1,60 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Container } from 'tailwind-react-ui';
+
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => {
   return {
-    currentUser: { id: state.reduxTokenAuth.currentUser.attributes.id }
+    currentUser: state.reduxTokenAuth.currentUser
   }
 }
 
 class UserProfileView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        organized_groups: []
+      }
+    };
+  }
   componentDidMount() {
-    // Get the number from url
-    debugger;
     console.log(this.props.currentUser)
+    this.getUser(this.props.currentUser.attributes.id)
+  }
+
+  async getUser(id) {
+    const credentials = { 'access-token': localStorage.getItem('access-token'), 'token-type': localStorage.getItem('token-type'), 'client': localStorage.getItem('client'), 'expiry': localStorage.getItem('expiry'), 'uid': localStorage.getItem('uid'), }
+
+    const response = await axios.get(`http://localhost:3000/users/${id}`, { headers: credentials })
+    const user = response.data.user
+    this.setState({ user });
   }
 
   render() {
+    let groups = this.state.user.organized_groups;
+    let groupList = groups.map(group => {
+      return (
+        <div key={group.id}>
+          <h3>
+            {group.name}
+          </h3>
+        </div>
+      )
+    })
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <>
+        <Container>
+          <h1>Hello {this.state.user.name}</h1>
+
+          <h2>You are the organizer of {groups.length} groups</h2>
+          {groupList}
+        </Container>
+
+      </>
     );
   }
 }
