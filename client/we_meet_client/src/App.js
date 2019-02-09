1484da3c81e96560cae8df3e79cd4e4a59bfc118
Jonathan <jon1969edwards@gmail.com>
2019-02-09 13:04:05 +0100

    Fixes merge issue

--- client/we_meet_client/src/App.js
+++ client/we_meet_client/src/App.js
@@ -2,24 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './css/tailwind.css';
 import axios from "axios";
-<<<<<<< HEAD
 import { Switch, Route, withRouter } from 'react-router-dom'
 import { signInUser, signOutUser, registerUser } from './redux-token-auth-config' 
 import NavBar from './components/ui-components/NavBar'
 import Footer from './components/ui-components/Footer'
 import MainView from './components/Views/MainView';
 import EventView from './components/Views/EventView';
-
-=======
-import { Switch, Route, withRouter } from 'react-router-dom';
-import { signInUser, signOutUser, registerUser } from './redux-token-auth-config';
-import NavBar from './components/ui-components/NavBar';
-import Footer from './components/ui-components/Footer';
-import MainView from './components/views/MainView';
-import EventView from './components/views/EventView';
-import GroupView from './components/views/GroupView';
-import CategoryView from './components/views/CategoryView';
->>>>>>> 23ad6cd910353c8fba25e47dbb021de3161a5dee
+import GroupView from './components/Views/GroupView';
+import CategoryView from './components/Views/CategoryView';
 class App extends Component {
 
   constructor(props) {
